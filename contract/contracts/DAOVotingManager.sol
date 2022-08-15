2d77c6cd5baa165f30119c46e37cfdda0ce385d9
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-08-15 20:32:05 +0200

    addOwner - creat transaction & execute

--- contract/contracts/DAOVotingManager.sol
+++ contract/contracts/DAOVotingManager.sol
@@ -2,46 +2,117 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@lukso/lsp-smart-contracts/contracts/LSP0ERC725Account/LSP0ERC725Account.sol";
 
 contract DAOVotingManager {
     using Counters for Counters.Counter;
 
     enum Vote {PENDING, YES, NO}
-    struct Transaction {
-        uint id;
+    enum OperationType {ADD_OWNER, REMOVE_OWNER, EXECUTE}
+
+    enum Status {PENDING, ACCEPTED, DECLINED}
+
+    struct Data {
+        OperationType operation;
+
+        //for EXECUTE
         bytes payload;
-        Vote[] votes;
+        address contractAddress;
+
+        //for ADD_OWNER and REMOVE_OWNER
+        uint newMinimumOwnersToExecute;
+        address owner;
     }
 
+    struct AddressVote {
+        Vote vote;
+        address owner;
+    }
+
+    struct Transaction {
+        uint id;
+        Data data;
+        AddressVote[] addressVotes;
+        Status status;
+    }
+
+    modifier onlyDaoOwners() {
+        bool isOwner = false;
+        for (uint i = 0; i < owners.length; i++) {
+            if (owners[i] == msg.sender) {
+                isOwner = true;
+                break;
+            }
+        }
+        require(isOwner, "Executor must be owner of a dao");
+        _;
+    }
+
+    modifier notAnOwner(address _address) {
+        bool isOwner = false;
+        for (uint i = 0; i < owners.length; i++) {
+            if (owners[i] == _address) {
+                isOwner = true;
+                break;
+            }
+        }
+        require(!isOwner, "Address cannot be an owner");
+        _;
+    }
+
+    LSP0ERC725Account account;
     uint public minimumOwnersToExecute;
     string public daoName;
     address[] internal owners;
     Counters.Counter internal transactionIdCounter;
-    mapping(uint => bytes) internal transactionIdToPayload;
+    mapping(uint => Data) internal transactionIdToData;
+    mapping(uint => Status) internal transactionIdToStatus;
     mapping(uint => mapping(address => Vote)) transactionIdToOwnerToVote;
 
     constructor(uint _minimumOwnersToExecute, string memory _daoName, address[] memory _owners) {
         minimumOwnersToExecute = _minimumOwnersToExecute;
         daoName = _daoName;
         owners = _owners;
+        account = new LSP0ERC725Account(address(this));
+    }
+
+    function addOwner(address _newOwner, uint _newMinimumOwnersToExecute) public onlyDaoOwners notAnOwner(_newOwner) {
+        uint ownersLength = owners.length;
+        require(_newMinimumOwnersToExecute <= ownersLength + 1, "New minimum owners to execute must be < owners count");
+
+        uint256 transactionId = transactionIdCounter.current();
+        Data memory data = Data(OperationType.ADD_OWNER, "", address(0), _newMinimumOwnersToExecute, _newOwner);
+
+        transactionIdCounter.increment();
+        transactionIdToData[transactionId] = data;
+        transactionIdToOwnerToVote[transactionId][msg.sender] = Vote.YES;
+        if(minimumOwnersToExecute == 1) {
+            addOwnerExecute(_newOwner, _newMinimumOwnersToExecute);
+            transactionIdToStatus[transactionId] = Status.ACCEPTED;
+        }
     }
 
     function getOwners() public view returns (address[] memory) {
         return owners;
     }
 
-    //TODO: test with some transactions
     function getTransactions() public view returns (Transaction[] memory) {
         uint counterValue = transactionIdCounter.current();
         uint ownersLength = owners.length;
         Transaction[] memory result = new Transaction[](counterValue);
         for (uint i = 0; i < counterValue; i++) {
-            Vote[] memory votes = new Vote[](ownersLength);
+            AddressVote[] memory addressVotes = new AddressVote[](ownersLength);
             for (uint j = 0; j < ownersLength; j++) {
-                votes[j] = transactionIdToOwnerToVote[i][owners[j]];
+                address owner = owners[j];
+                addressVotes[j] = AddressVote(transactionIdToOwnerToVote[i][owner], owner);
             }
-            result[i] = Transaction(i, transactionIdToPayload[i], votes);
+            result[i] = Transaction(i, transactionIdToData[i], addressVotes, transactionIdToStatus[i]);
         }
         return result;
+    }
+
+    function addOwnerExecute(address _newOwner, uint _newMinimumOwnersToExecute) internal {
+        owners.push(_newOwner);
+        minimumOwnersToExecute = _newMinimumOwnersToExecute;
     }
 }
--- contract/test/DAOVotingManager.test.js
+++ contract/test/DAOVotingManager.test.js
@@ -8,7 +8,7 @@ describe("DAOVotingManager", function () {
         return await DAOVotingManager.deploy(minimumOwnersToExecute, daoName, owners)
     }
 
-    it("should deploy", async function () {
+    it("should deploy", async () => {
         const [owner, address1, address2] = await ethers.getSigners();
         const minimumOwnersToExecute = 3
         const daoName = "fashionDao"
@@ -21,6 +21,92 @@ describe("DAOVotingManager", function () {
         expect(await contract.daoName()).to.be.equal(daoName)
         expect(await contract.getOwners()).to.deep.equal(owners)
         expect((await contract.getTransactions()).length).to.be.equal(0)
+    });
+
+    it("should fail to add owner when executed by not owner", async () => {
+        const [owner, address1, address2, address3, address4] = await ethers.getSigners();
+        const minimumOwnersToExecute = 3
+        const daoName = "fashionDao"
+        const owners = [owner.address, address1.address, address2.address]
+        const contract = await deployContract(minimumOwnersToExecute, daoName, owners)
+
+        const addOwnerTx = contract.connect(address3).addOwner(address4.address, 1);
+
+        await expect(addOwnerTx).to.be.revertedWith("Executor must be owner of a dao")
+    });
+
+    it("should fail to add owner when newMinimumOwnersToExecute > owners", async () => {
+        const [owner, address1, address2, address3] = await ethers.getSigners();
+        const minimumOwnersToExecute = 3
+        const daoName = "fashionDao"
+        const owners = [owner.address, address1.address, address2.address]
+        const contract = await deployContract(minimumOwnersToExecute, daoName, owners)
+
+        const addOwnerTx = contract.addOwner(address3.address, 5);
+
+        await expect(addOwnerTx).to.be.revertedWith("New minimum owners to execute must be < owners count")
+    });
+
+    it("should fail to add owner when address is an owner", async () => {
+        const [owner, address1, address2, address3] = await ethers.getSigners();
+        const minimumOwnersToExecute = 3
+        const daoName = "fashionDao"
+        const owners = [owner.address, address1.address, address2.address]
+        const contract = await deployContract(minimumOwnersToExecute, daoName, owners)
+
+        const addOwnerTx = contract.addOwner(address2.address, 4);
+
+        await expect(addOwnerTx).to.be.revertedWith("Address cannot be an owner")
+    });
+
+    it("should add owner create proposal", async () => {
+        const [owner, address1, address2, address3] = await ethers.getSigners();
+        const minimumOwnersToExecute = 3
+        const daoName = "fashionDao"
+        const owners = [owner.address, address1.address, address2.address]
+        const contract = await deployContract(minimumOwnersToExecute, daoName, owners)
+
+        await contract.addOwner(address3.address, 4);
+
+        const transactions = await contract.getTransactions()
+        expect(transactions.length).to.be.equal(1)
+        expect(transactions[0].id).to.be.equal(0)
+        expect(transactions[0].status).to.be.equal(0)
+        expect(transactions[0].data.operation).to.be.equal(0)
+        expect(transactions[0].data.payload).to.be.equal("0x")
+        expect(transactions[0].data.contractAddress).to.be.equal("0x0000000000000000000000000000000000000000")
+        expect(transactions[0].data.newMinimumOwnersToExecute).to.be.equal(4)
+        expect(transactions[0].data.owner).to.be.equal(address3.address)
+        expect(transactions[0].addressVotes.length).to.be.equal(3)
+        expect(transactions[0].addressVotes[0].vote).to.be.equal(1)
+        expect(transactions[0].addressVotes[0].owner).to.be.equal(owner.address)
+        expect(transactions[0].addressVotes[1].vote).to.be.equal(0)
+        expect(transactions[0].addressVotes[1].owner).to.be.equal(address1.address)
+        expect(transactions[0].addressVotes[2].vote).to.be.equal(0)
+        expect(transactions[0].addressVotes[2].owner).to.be.equal(address2.address)
+    });
+
+    it("should add owner create proposal and execute", async () => {
+        const [owner, address1] = await ethers.getSigners();
+        const minimumOwnersToExecute = 1
+        const daoName = "fashionDao"
+        const owners = [owner.address]
+        const contract = await deployContract(minimumOwnersToExecute, daoName, owners)
+
+        await contract.addOwner(address1.address, 2);
+
+        expect(await contract.getOwners()).to.deep.equal([owner.address, address1.address])
+        expect(await contract.minimumOwnersToExecute()).to.be.equal(2)
+
+        const transactions = await contract.getTransactions()
+        expect(transactions.length).to.be.equal(1)
+        expect(transactions[0].id).to.be.equal(0)
+        expect(transactions[0].status).to.be.equal(1)
+        expect(transactions[0].data.operation).to.be.equal(0)
+        expect(transactions[0].data.payload).to.be.equal("0x")
+        expect(transactions[0].data.contractAddress).to.be.equal("0x0000000000000000000000000000000000000000")
+        expect(transactions[0].data.newMinimumOwnersToExecute).to.be.equal(2)
+        expect(transactions[0].data.owner).to.be.equal(address1.address)
     });
 
 });
