3cb9e30ce038bfc4f7a366cc98981aca499c0052
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-08-15 14:29:29 +0200

    contract: minimum owners, dao name, owners

--- contract/contracts/DAOVotingManager.sol
+++ contract/contracts/DAOVotingManager.sol
@@ -3,4 +3,18 @@ pragma solidity ^0.8.9;
 
 contract DAOVotingManager {
 
+    uint public minimumOwnersToExecute;
+    string public daoName;
+    address[] internal owners;
+
+    constructor(uint _minimumOwnersToExecute, string memory _daoName, address[] memory _owners) {
+        minimumOwnersToExecute = _minimumOwnersToExecute;
+        daoName = _daoName;
+        owners = _owners;
+    }
+
+    function getOwners() public view returns (address[] memory) {
+        return owners;
+    }
+
 }
--- contract/test/DAOVotingManager.test.js
+++ contract/test/DAOVotingManager.test.js
@@ -3,13 +3,23 @@ const {ethers} = require("hardhat");
 
 describe("DAOVotingManager", function () {
 
-    const deployContract = async () => {
-        const DAOVotingManager = await ethers.getContractFactory("DAOVotingManager");
-        return await DAOVotingManager.deploy();
+    const deployContract = async (minimumOwnersToExecute, daoName, owners) => {
+        const DAOVotingManager = await ethers.getContractFactory("DAOVotingManager")
+        return await DAOVotingManager.deploy(minimumOwnersToExecute, daoName, owners)
     }
 
     it("should deploy", async function () {
-        await deployContract();
+        const [owner, address1, address2] = await ethers.getSigners();
+        const minimumOwnersToExecute = 3
+        const daoName = "fashionDao"
+        const owners = [owner.address, address1.address, address2.address]
+
+
+        const contract = await deployContract(minimumOwnersToExecute, daoName, owners)
+
+        expect(await contract.minimumOwnersToExecute()).to.be.equal(3)
+        expect(await contract.daoName()).to.be.equal(daoName)
+        expect(await contract.getOwners()).to.deep.equal(owners)
     });
 
 });
