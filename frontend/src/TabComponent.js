af9e9cac27c28ca6b45062eae57cbc0cd39a077b
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-08-16 16:00:23 +0200

    improve menu css

--- frontend/src/TabComponent.js
+++ frontend/src/TabComponent.js
@@ -1,26 +1,35 @@
 import {Tab, Tabs} from "react-bootstrap";
 import {useNavigate} from "react-router";
+import "./TabComponent.css"
 
 export default function TabComponent({active}) {
 
     const navigate = useNavigate();
 
-    return <Tabs
-        defaultActiveKey={active}
-        className="mb-3"
-        fill
-        onSelect={(e, b) => {
-            if(e === "manage") {
-                navigate("/")
-            } else {
-                navigate("/deploy")
-            }
-        }}>
-        <Tab eventKey="manage" title="Manage">
-            AAA
-        </Tab>
-        <Tab eventKey="deploy" title="Deploy">
-            BBB
-        </Tab>
-    </Tabs>
+    const onSelect = (tab) => {
+        if (tab === "manage") {
+            navigate("/")
+        } else {
+            navigate("/deploy")
+        }
+    }
+
+    return <div>
+        <div className={"header"}>
+            DAO Voting Key Manager
+        </div>
+        <div className={"tabComponent"}>
+            <Tabs
+                defaultActiveKey={active}
+                fill
+                onSelect={onSelect}>
+                <Tab eventKey="manage" title="Manage">
+                    AAA
+                </Tab>
+                <Tab eventKey="deploy" title="Deploy">
+                    BBB
+                </Tab>
+            </Tabs>
+        </div>
+    </div>
 }
