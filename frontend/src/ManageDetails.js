b43b7dd90c33d62ea6bbeb2da80e9ca66fa8868a
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-08-22 13:28:11 +0200

    move overview data to parent component

--- frontend/src/ManageDetails.js
+++ frontend/src/ManageDetails.js
@@ -11,6 +11,9 @@ import Overview from "./ManageOverview";
 import ManageDeposit from "./ManageDeposit";
 import ManageProposalList from "./ManageProposalList";
 import ManageWithdraw from "./ManageWithdraw";
+import {ERC725YKeys} from "@lukso/lsp-smart-contracts/constants";
+import {toUtf8String} from "@ethersproject/strings";
+import LSP7DigitalAsset from "@lukso/lsp-smart-contracts/artifacts/LSP7DigitalAsset.json";
 
 export default function ManageDetails({myAddress, signer, provider, activeKey}) {
 
@@ -20,12 +23,44 @@ export default function ManageDetails({myAddress, signer, provider, activeKey}) 
     const [contract, setContract] = useState()
     const [reloadCounter, setReloadCounter] = useState(0)
 
+    const [daoName, setDaoName] = useState()
+    const [governanceTokenAddress, setGovernanceTokenAddress] = useState()
+    const [governanceTokenSymbol, setGovernanceTokenSymbol] = useState()
+    const [governanceTokenName, setGovernanceTokenName] = useState()
+    const [daoAccountAddress, setDaoAccountAddress] = useState()
+    const [tokensToCreateProposal, setTokensToCreateProposal] = useState()
+    const [minimumTokensToExecuteProposal, setMinimumTokensToExecuteProposal] = useState()
+    const [proposalTimeToVote, setProposalTimeToVote] = useState()
+    const [currentBalance, setCurrentBalance] = useState()
+
     let {address} = useParams();
+
+    const initialize = (contract) => {
+        contract.daoName().then(name => setDaoName(name))
+        contract.daoGovernanceToken().then(tokenAddress => {
+            setGovernanceTokenAddress(tokenAddress)
+            return tokenAddress
+        }).then(tokenAddress => {
+            const contract = ContractFactory.getContract(tokenAddress, LSP7DigitalAsset.abi, provider)
+            contract["getData(bytes32)"](ERC725YKeys.LSP4.LSP4TokenName).then(tokenName => setGovernanceTokenName(toUtf8String(tokenName)))
+            contract["getData(bytes32)"](ERC725YKeys.LSP4.LSP4TokenSymbol).then(tokenSymbol => setGovernanceTokenSymbol(toUtf8String(tokenSymbol)))
+        })
+        contract.account().then(address => {
+            setDaoAccountAddress(address)
+            provider.getBalance(address).then(balance => setCurrentBalance(balance))
+        })
+        contract.tokensToCreateProposal().then(tokens => setTokensToCreateProposal(ethers.utils.formatEther(tokens)))
+        contract.minTokensToExecuteProposal().then(tokens => setMinimumTokensToExecuteProposal(ethers.utils.formatEther(tokens)))
+        contract.proposalTimeToVoteInSeconds().then(proposalTimeToVoteInSeconds => setProposalTimeToVote(proposalTimeToVoteInSeconds.toNumber()))
+    }
 
     useEffect(_ => {
         if (myAddress && signer && provider) {
             console.log("Initializing Manage Details")
-            updateIsValidContract().then(_ => updateContract())
+            updateIsValidContract().then(_ => {
+                const contract = updateContract()
+                initialize(contract)
+            })
         }
     }, [myAddress, signer, provider, reloadCounter])
 
@@ -45,6 +80,7 @@ export default function ManageDetails({myAddress, signer, provider, activeKey}) 
     const updateContract = () => {
         const contract = ContractFactory.getContract(address, DaoVotingManager.abi, signer)
         setContract(contract)
+        return contract
     }
 
     if (!address || !signer || !provider) {
@@ -55,39 +91,60 @@ export default function ManageDetails({myAddress, signer, provider, activeKey}) 
         return <div className={"connectWallet"}>Provided address is not a DAO Key Manager</div>
     }
 
+    const overviewSection = () => {
+        if (!daoName || !governanceTokenAddress || !governanceTokenSymbol || !governanceTokenName || !daoAccountAddress
+            || !tokensToCreateProposal || !minimumTokensToExecuteProposal || !proposalTimeToVote || !address || !currentBalance) {
+            return null
+        }
+
+        return <Overview daoName={daoName} governanceTokenAddress={governanceTokenAddress}
+                         governanceTokenSymbol={governanceTokenSymbol}
+                         governanceTokenName={governanceTokenName}
+                         daoAccountAddress={daoAccountAddress}
+                         tokensToCreateProposal={tokensToCreateProposal}
+                         minimumTokensToExecuteProposal={minimumTokensToExecuteProposal}
+                         proposalTimeToVote={proposalTimeToVote} currentBalance={currentBalance}/>
+    }
+
     if (isValidContract && contract) {
         return <div className={"manageDetails"}>
             <Tab.Container activeKey={activeKey}>
                 <Row>
                     <Col sm={2}>
-                        <Nav variant="pills" className="flex-column roundedBorders" >
+                        <Nav variant="pills" className="flex-column roundedBorders">
                             <Nav.Item className={"leftTab"}>
-                                <Nav.Link eventKey="overview" onClick={_ => navigate("/manage/" + address)}>Overview</Nav.Link>
+                                <Nav.Link eventKey="overview"
+                                          onClick={_ => navigate("/manage/" + address)}>Overview</Nav.Link>
                             </Nav.Item>
                             <Nav.Item className={"leftTab"}>
-                                <Nav.Link eventKey="deposit" onClick={_ => navigate("/manage/" + address + "/deposit")}>Deposit</Nav.Link>
+                                <Nav.Link eventKey="deposit"
+                                          onClick={_ => navigate("/manage/" + address + "/deposit")}>Deposit</Nav.Link>
                             </Nav.Item>
                             <Nav.Item className={"leftTab"}>
-                                <Nav.Link eventKey="withdraw" onClick={_ => navigate("/manage/" + address + "/withdraw")}>Withdraw</Nav.Link>
+                                <Nav.Link eventKey="withdraw"
+                                          onClick={_ => navigate("/manage/" + address + "/withdraw")}>Withdraw</Nav.Link>
                             </Nav.Item>
                             <Nav.Item className={"leftTab"}>
-                                <Nav.Link eventKey="proposals" onClick={_ => navigate("/manage/" + address + "/proposals")}>Proposals</Nav.Link>
+                                <Nav.Link eventKey="proposals"
+                                          onClick={_ => navigate("/manage/" + address + "/proposals")}>Proposals</Nav.Link>
                             </Nav.Item>
                         </Nav>
                     </Col>
                     <Col sm={10}>
                         <Tab.Content>
                             <Tab.Pane eventKey="overview">
-                                <Overview contract={contract} provider={provider}/>
+                                {overviewSection()}
                             </Tab.Pane>
                             <Tab.Pane eventKey="deposit">
-                                <ManageDeposit contract={contract} signer={signer} currentAddress={myAddress} reloadParent={reload} reloadCounter={reloadCounter}/>
+                                <ManageDeposit contract={contract} signer={signer} currentAddress={myAddress}
+                                               reloadParent={reload} reloadCounter={reloadCounter}/>
                             </Tab.Pane>
                             <Tab.Pane eventKey="withdraw">
-                                <ManageWithdraw />
+                                <ManageWithdraw/>
                             </Tab.Pane>
                             <Tab.Pane eventKey="proposals">
-                                <ManageProposalList contract={contract} signer={signer} currentAddress={myAddress} provider={provider} reloadCounter={reloadCounter}/>
+                                <ManageProposalList contract={contract} signer={signer} currentAddress={myAddress}
+                                                    provider={provider} reloadCounter={reloadCounter}/>
                             </Tab.Pane>
                         </Tab.Content>
                     </Col>
--- frontend/src/ManageOverview.js
+++ frontend/src/ManageOverview.js
@@ -1,11 +1,7 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {useEffect, useState} from "react";
-import LSP7DigitalAsset from "@lukso/lsp-smart-contracts/artifacts/LSP7DigitalAsset.json";
 import "@lukso/lsp-smart-contracts/contracts/LSP4DigitalAssetMetadata/LSP4Constants.sol";
-import {ContractFactory, ethers} from "ethers";
-import {ERC725YKeys} from "@lukso/lsp-smart-contracts/constants";
-import {toUtf8String} from "@ethersproject/strings";
+import {ethers} from "ethers";
 import "./ManageOverview.css"
 import {useParams} from "react-router";
 
@@ -13,71 +9,43 @@ const SECONDS_IN_MINUTE = 60
 const SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60
 const SECONDS_IN_DAYS = SECONDS_IN_HOUR * 24
 
-export default function Overview({contract, provider}) {
+export default function Overview({
+                                     daoName,
+                                     governanceTokenAddress,
+                                     governanceTokenSymbol,
+                                     governanceTokenName,
+                                     daoAccountAddress,
+                                     tokensToCreateProposal,
+                                     minimumTokensToExecuteProposal,
+                                     proposalTimeToVote,
+                                     currentBalance
+                                 }) {
 
     let {address} = useParams();
 
-    const [daoName, setDaoName] = useState()
-    const [governanceTokenAddress, setGovernanceTokenAddress] = useState()
-    const [governanceTokenSymbol, setGovernanceTokenSymbol] = useState()
-    const [governanceTokenName, setGovernanceTokenName] = useState()
-    const [daoAccountAddress, setDaoAccountAddress] = useState()
-    const [tokensToCreateProposal, setTokensToCreateProposal] = useState()
-    const [minimumTokensToExecuteProposal, setMinimumTokensToExecuteProposal] = useState()
-    const [proposalTimeToVote, setProposalTimeToVote] = useState()
-    const [currentBalance, setCurrentBalance] = useState()
-
-    const initialize = () => {
-        contract.daoName().then(name => setDaoName(name))
-        contract.daoGovernanceToken().then(tokenAddress => {
-            setGovernanceTokenAddress(tokenAddress)
-            return tokenAddress
-        }).then(tokenAddress => {
-            const contract = ContractFactory.getContract(tokenAddress, LSP7DigitalAsset.abi, provider)
-            contract["getData(bytes32)"](ERC725YKeys.LSP4.LSP4TokenName).then(tokenName => setGovernanceTokenName(toUtf8String(tokenName)))
-            contract["getData(bytes32)"](ERC725YKeys.LSP4.LSP4TokenSymbol).then(tokenSymbol => setGovernanceTokenSymbol(toUtf8String(tokenSymbol)))
-        })
-        contract.account().then(address => {
-            setDaoAccountAddress(address)
-            provider.getBalance(address).then(balance => setCurrentBalance(balance))
-        })
-        contract.tokensToCreateProposal().then(tokens => setTokensToCreateProposal(ethers.utils.formatEther(tokens)))
-        contract.minTokensToExecuteProposal().then(tokens => setMinimumTokensToExecuteProposal(ethers.utils.formatEther(tokens)))
-        contract.proposalTimeToVoteInSeconds().then(proposalTimeToVoteInSeconds => setProposalTimeToVote(proposalTimeToVoteInSeconds.toNumber()))
-    }
-
-    useEffect(_ => {
-        initialize()
-    }, [])
-
     const renderProposalTimeToVote = (timeToVoteInSeconds) => {
-        if(timeToVoteInSeconds % SECONDS_IN_DAYS === 0) {
+        if (timeToVoteInSeconds % SECONDS_IN_DAYS === 0) {
             const days = "" + (timeToVoteInSeconds / SECONDS_IN_DAYS)
-            if(days === "1") {
+            if (days === "1") {
                 return days + " Day"
             } else {
                 return days + " Days"
             }
-        } else if(timeToVoteInSeconds % SECONDS_IN_HOUR === 0) {
+        } else if (timeToVoteInSeconds % SECONDS_IN_HOUR === 0) {
             const hours = "" + (timeToVoteInSeconds / SECONDS_IN_HOUR)
-            if(hours === "1") {
+            if (hours === "1") {
                 return hours + " Hour"
             } else {
                 return hours + " Hour"
             }
         } else {
             const minutes = "" + (timeToVoteInSeconds / SECONDS_IN_MINUTE)
-            if(minutes === "1") {
+            if (minutes === "1") {
                 return minutes + " Minute"
             } else {
                 return minutes + " Minutes"
             }
         }
-    }
-
-    if (!daoName || !governanceTokenAddress || !governanceTokenSymbol || !governanceTokenName || !daoAccountAddress
-        || !tokensToCreateProposal || !minimumTokensToExecuteProposal || !proposalTimeToVote || !address || !currentBalance) {
-        return null
     }
 
     const name = () => <div className={"manageSection"}>
